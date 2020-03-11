b91586e27e969a3c320b877483415dc9c765984e
Theo Booth <theorlbooth@googlemail.com>
2020-03-11 17:27:59 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -13,14 +13,16 @@ function checkCollision(rock) {
   const top = positionToInteger(rock.style.top)
   if (top > 360) {
     const dodgerLeftEdge = positionToInteger(DODGER.style.left)
-    const dodgerRightEdge = `${dodgerLeftEdge += 40}px`;
+    const dodgerRightEdge = dodgerLeftEdge + 40;
     const rockLeftEdge = positionToInteger(rock.style.left)
-    const rockRightEdge = `${rockLeftEdge += 20}px`;
+    const rockRightEdge = rockLeftEdge + 20;
     if ((rockLeftEdge < dodgerLeftEdge && rockRightEdge > dodgerLeftEdge) ||
        (rockLeftEdge > dodgerLeftEdge && rockRightEdge < dodgerRightEdge) ||
        (rockLeftEdge < dodgerRightEdge && rockRightEdge > dodgerRightEdge)) {
       return true
     }
+  } else {
+    return false
   }
 }
 
